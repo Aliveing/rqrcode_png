e9bc18d9827dae62d614b8db47255ad6d1680f4e
Alive <appreach_ing@hotmail.com>
2019-10-14 12:26:23 +0800

    Update sequence.rb
    rqrcode upgrade to 1.1.1

--- lib/rqrcode_png/sequence.rb
+++ lib/rqrcode_png/sequence.rb
@@ -8,7 +8,7 @@ module RQRCodePNG
     def dark_squares_only(&block)
       @qr_code.modules.each_index do |row|
         @qr_code.modules.each_index do |column|
-          if @qr_code.dark?(row, column)
+          if @qr_code.checked?(row, column)
             yield row, column
           end
         end
